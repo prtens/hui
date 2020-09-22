00f7b40b2bd81b0631c30c3700879c4f3f4ddaaf
prens <1017098134@qq.com>
2020-09-22 10:21:16 +0800

    fixL

--- src/components/back-top/back-top.vue
+++ src/components/back-top/back-top.vue
@@ -11,9 +11,8 @@
         height="16"
         viewBox="0 0 17 17"
         xmlns="http://www.w3.org/2000/svg"
-        class="hn-back-top__icon"
+        class="hn-back-top--icon"
         aria-hidden="true"
-        style="height:16px;width:16px"
       >
         <path d="M12.036 15.59a1 1 0 0 1-.997.995H5.032a.996.996 0 0 1-.997-.996V8.584H1.03c-1.1 0-1.36-.633-.578-1.416L7.33.29a1.003 1.003 0 0 1 1.412 0l6.878 6.88c.782.78.523 1.415-.58 1.415h-3.004v7.004z" />
       </svg>
@@ -41,9 +40,7 @@ export default {
           bottom: "50px",
           width: "40px",
           height: "40px",
-          "border-radius": "4px",
-          "line-height": "45px",
-          background: "#e7eaf1"
+          "line-height": "45px"
         };
       }
     },
