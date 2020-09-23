f32dfa9701402211e003557a46d46ebc0b5110bd
prens <1017098134@qq.com>
2020-09-23 18:54:08 +0800

    [build] 0.1.8

--- build/bin/release.sh
+++ build/bin/release.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env sh
+
 echo ">>>>>> start push <<<<<<"
 echo " ====== 当前分支 ====== "
 
-branch= git branch
+git checkout master
 
-echo $branch
+VERSION=`npx select-version-cli`
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  # build
+  VERSION=$VERSION npm run dist
+
+  # publish
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push -u origin master
+  git push
+  npm publish
+fi
