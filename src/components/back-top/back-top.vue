6b5f4247dd713b1d8cae1540879e045fdddc57c9
prtens <hux@huonu.com>
2020-09-30 14:30:03 +0800

    fix

--- src/components/back-top/back-top.vue
+++ src/components/back-top/back-top.vue
@@ -6,16 +6,7 @@
       class="hn-back-top"
       @click="backToTop"
     >
-      <svg
-        width="16"
-        height="16"
-        viewBox="0 0 17 17"
-        xmlns="http://www.w3.org/2000/svg"
-        class="hn-back-top--icon"
-        aria-hidden="true"
-      >
-        <path d="M12.036 15.59a1 1 0 0 1-.997.995H5.032a.996.996 0 0 1-.997-.996V8.584H1.03c-1.1 0-1.36-.633-.578-1.416L7.33.29a1.003 1.003 0 0 1 1.412 0l6.878 6.88c.782.78.523 1.415-.58 1.415h-3.004v7.004z" />
-      </svg>
+      <h-svg-icon icon-class="back-top"/>
     </div>
   </transition>
 </template>
